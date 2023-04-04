05406aca2085ec36ac3049a86b0311248fbadf90
Tev-J <tj_delph@yahoo.com>
2023-04-04 14:48:52 -0400

    complete lab

---
+++ index.js
@@ -0,0 +1,37 @@
+function Monday() {
+  console.log("Go for a five-mile run");
+  console.log("Pump iron");
+}
+
+function Tuesday() {
+  console.log("Go for a five-mile run");
+  console.log("Swim 40 laps");
+}
+
+function Wednesday() {
+  console.log("Go for a five-mile run");
+  console.log("Go for a five-mile run");
+}
+
+function Thursday() {
+  console.log("Go for a five-mile run");
+  console.log("Pump iron");
+}
+
+function Friday() {
+  console.log("Go for a five-mile run");
+  console.log("Swim 40 laps");
+}
+
+function receivesAFunction(callback) {
+  return callback();
+}
+
+function returnsANamedFunction() {
+  const example = () => console.log("hi");
+  return example;
+}
+
+function returnsAnAnonymousFunction() {
+  return () => {};
+}
